a295367b5ea54cc2bf058c7cfbd25de6cbf25c42
vincenzandrin <andrin.vincenz@siemens.com>
2022-05-31 15:38:20 +0200

    changed

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -7,7 +7,8 @@ export const HEROES: Hero[] = [
   { id: 15, name: 'Magneta' },
   { id: 16, name: 'RubberMan' },
   { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr. IQ' },
+  { id: 18, name: 'Dr. Mama' },
   { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 20, name: 'Tornado' },
+  { id: 21, name: 'Smoker'}
 ];
